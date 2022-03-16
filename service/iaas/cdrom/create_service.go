a95c1fbba02161f014b829e41bc9dde5618fa4d0
Kazumichi Yamamoto <yamamoto.febc@gmail.com>
2022-03-16 09:47:36 +0900

    service: (#25)
    - bill
    - bridge
    - cdrom

---
+++ service/iaas/cdrom/create_service.go
@@ -0,0 +1,73 @@
+// Copyright 2022 The sacloud/sacloud-go Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cdrom
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/sacloud/ftps"
+	"github.com/sacloud/iaas-api-go"
+	"github.com/sacloud/sacloud-go/pkg/size"
+)
+
+func (s *Service) Create(req *CreateRequest) (*iaas.CDROM, error) {
+	return s.CreateWithContext(context.Background(), req)
+}
+
+func (s *Service) CreateWithContext(ctx context.Context, req *CreateRequest) (*iaas.CDROM, error) {
+	if err := req.Validate(); err != nil {
+		return nil, err
+	}
+
+	var reader io.Reader
+	switch req.SourcePath {
+	case "":
+		reader = req.SourceReader
+	default:
+		file, err := os.Open(req.SourcePath)
+		if err != nil {
+			return nil, fmt.Errorf("reading source file[%s] failed: %s", req.SourcePath, err)
+		}
+		defer file.Close() // nolint
+		reader = file
+	}
+
+	client := iaas.NewCDROMOp(s.caller)
+	cdrom, ftpServer, err := client.Create(ctx, req.Zone, &iaas.CDROMCreateRequest{
+		SizeMB:      req.SizeGB * size.GiB,
+		Name:        req.Name,
+		Description: req.Description,
+		Tags:        req.Tags,
+		IconID:      req.IconID,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	ftpsClient := ftps.NewClient(ftpServer.User, ftpServer.Password, ftpServer.HostName)
+	if err := ftpsClient.UploadReader("data.iso", reader); err != nil {
+		return nil, err
+	}
+
+	if err := client.CloseFTP(ctx, req.Zone, cdrom.ID); err != nil {
+		return nil, err
+	}
+
+	// refresh
+	return client.Read(ctx, req.Zone, cdrom.ID)
+}
